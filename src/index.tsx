d04ca3120aa030769aae6a09bd7be01133586eef
Yuan <faryang@sohu-inc.com>
2018-01-23 14:54:45 +0800

    update app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,36 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Route, HashRouter as Router, Link } from 'react-router-dom';
 import App from './App/App';
-import registerServiceWorker from './registerServiceWorker';
 
+import registerServiceWorker from './registerServiceWorker';
 import './common/style.less';
 
+type IRoute = {
+  link: string,
+  exact?: boolean,
+  icon?: string,
+  component?: any,
+  subRoutes?: Array<IRoute>
+};
+
+const appPage: Array<IRoute> = [
+  {
+      link: '/',
+      component: App
+  }
+];
 ReactDOM.render(
-  <App />,
+  <Router>
+    {
+      appPage.map((item, index) => (
+        <Route
+          path={item.link}
+          component={item.component}
+        />
+      ))
+    }
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
