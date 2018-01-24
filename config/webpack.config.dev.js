9f389fa3289c68db9d259bc50160ef4acf6ef3a6
Yuan <faryang@sohu-inc.com>
2018-01-24 16:53:50 +0800

    解决因为less-loader顺序问题引发的编译失败

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -168,9 +168,6 @@ module.exports = {
                 },
               },
               {
-                loader: require.resolve('less-loader'),
-              },
-              {
                 loader: require.resolve('postcss-loader'),
                 options: {
                   // Necessary for external CSS imports to work
@@ -189,6 +186,9 @@ module.exports = {
                     }),
                   ],
                 },
+              },
+              {
+                loader: require.resolve('less-loader'),
               },
             ],
           },
