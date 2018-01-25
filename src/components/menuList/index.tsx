fd6e5cfa3abd4cfed2c915751be9a2145217f932
杨远 <yangyuan@xiyou3g.com>
2018-01-25 22:33:02 +0800

    add menuList

--- src/components/menuList/index.tsx
+++ src/components/menuList/index.tsx
@@ -1,11 +1,47 @@
 import * as React from 'react';
-import menuList from './data';
+import { Link } from 'react-router-dom';
+
+import { menuList, menuFeature } from './data';
+import './style.less';
+const logo = require('./favicon.png');
 
 class MenuList extends React.Component {
     render() {
-        return menuList.map((item, index) => (
-            <div>{item.label}</div>
-        ));
+        return (
+            <div className="menu-wrapper">
+                <div className="menu-aside">
+                    <div className="menu-aside-header">
+                        <h3>Xiyou</h3>
+                    </div>
+                    <ul className="menu-basic">
+                        {
+                            menuList.map((item, index) => (
+                                <li className="menu-list" key={index}>
+                                    <Link to={item.link}>
+                                        <i className={item.icon} />
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    <ul className="menu-feature">
+                    {
+                            menuFeature.map((item, index) => (
+                                <li className="menu-list" key={index}>
+                                    <Link to={item.link}>
+                                        <i className={item.icon} />
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                    <div className="menu-aside-footer">
+                        <img src={logo} />
+                    </div>
+                </div>
+                <div className="menu-content">content</div>
+            </div>
+        );
     }
 }
 
