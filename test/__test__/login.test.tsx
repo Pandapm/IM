1c17a658bb3dd618a32a9179f0497b7558663fe0
Yuan <faryang@sohu-inc.com>
2018-02-07 11:57:19 +0800

    update

--- test/__test__/login.test.tsx
+++ test/__test__/login.test.tsx
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import Login from '../../src/components/login/index';
 
 describe('Login Component', () => {
+    // 判断组件是否正常渲染
     it('login Component should be render', () => {
         shallow(<Login />);
     });
