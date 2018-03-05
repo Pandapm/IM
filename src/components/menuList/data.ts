444ecd547705e9d9b9fdec39e585e3e1260b6f09
Yuan <faryang@sohu-inc.com>
2018-03-05 16:15:50 +0800

    update menu stylesheets

--- src/components/menuList/data.ts
+++ src/components/menuList/data.ts
@@ -8,27 +8,27 @@ export const menuList: Array<XMenu> = [
     {
         icon: 'icon icon-bubbles3',
         label: 'IM聊天',
-        link: '/menu/chat',
+        link: '/chat',
     },
     {
         icon: 'icon icon-file-text2',
         label: 'WIKI信息',
-        link: '/menu/wiki',
+        link: '/wiki',
     },
     {
         icon: 'icon icon-fire',
         label: '相关动态',
-        link: '/menu/active',
+        link: '/active',
     },
     {
         icon: 'icon icon-download2',
         label: '应用下载',
-        link: '/menu/download',
+        link: '/download',
     },
     {
         icon: ' icon icon-wink',
         label: '成员信息',
-        link: '/menu/member',
+        link: '/member',
     }
 ];
 
@@ -36,12 +36,12 @@ export const menuFeature: Array<XMenu> = [
     {
         icon: 'icon icon-stack',
         label: '我的wiki',
-        link: '/menu/manage/wiki',
+        link: '/manage/wiki',
     },
     {
         icon: 'icon icon-upload2',
         label: '应用上传',
-        link: '/menu/manage/application',
+        link: '/manage/application',
     }
 ];
 
@@ -49,21 +49,21 @@ export const infoFeature: Array<XMenu> = [
     {
         icon: 'icon icon-delicious',
         label: '动态管理',
-        link: '/menu/manage/active',
+        link: '/manage/active',
     },
     {
         icon: 'icon icon-users',
         label: '成员管理',
-        link: '/menu/manage/users',
+        link: '/manage/users',
     },
     {
         icon: 'icon icon-library',
         label: '信息管理',
-        link: '/menu/manage/info',
+        link: '/manage/info',
     },
     {
         icon: 'icon icon-magic-wand',
         label: '任务分发',
-        link: '/menu/manage/plan'
+        link: '/manage/plan'
     }
 ];
