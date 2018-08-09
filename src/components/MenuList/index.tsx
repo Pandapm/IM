7d9ba65e823c12bdbbf5744964e0ac8f7df4129a
JunYing <junying@pinduoduo.com>
2018-08-09 14:40:56 +0800

    refactor(Comp: MenuList): 重写样式，采用b-e-m命名规范

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -1,35 +1,36 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { menuList, menuFeature, infoFeature } from '../../common/data';
 import Menu from './menu';
 import Main from '../Main';
+import { menuList, menuFeature, infoFeature } from '../../common/data';
 import './style.less';
 const logo = require('./favicon.png');
+const prefixCls = 'menu-wrapper'; 
 
 const MenuList = () => (
-    <div className="menu-wrapper">
-        <div className="menu-aside">
-            <div className="menu-aside-header">
+    <div className={prefixCls}>
+        <div className={`${prefixCls}-aside`}>
+            <div className={`${prefixCls}-aside-header`}>
                 <NavLink to={'/'}>
-                    <h3>Xiyou</h3>
+                    <h3>X-More</h3>
                 </NavLink>
             </div>
             <Menu
-                clsName={'menu-basic'}
+                clsName={`${prefixCls}-content-basic`}
                 list={menuList}
             />
             <Menu
-                clsName={'menu-feature'}
+                clsName={`${prefixCls}-content-feature`}
                 list={menuFeature}
             />
             <Menu
-                clsName={'info-feature'}
+                clsName={`${prefixCls}-content-info`}
                 list={infoFeature}
             />
-            <a className="menu-aside-footer">
+            <a className={`${prefixCls}-footer`}>
                 <img src={logo} />
-                <p className="menu-aside-footer-name">杨远</p>
+                <p className={`${prefixCls}-footer-name`}>杨远</p>
             </a>
         </div>
         <Main />
