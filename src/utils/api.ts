7576782a2819d2bf11123cf48da5a271f5dbe9c0
JunYing <junying@pinduoduo.com>
2018-08-16 22:52:34 +0800

    feat(API): 添加API 前端统一拦截配置

---
+++ src/utils/api.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import baseConfig from './baseConfig';
+
+let pending: Array<{
+    url: string,
+    cancel: Function,
+}>;
+
+const removePending = config => {
+    for (let p in pending) {
+        if (pending.hasOwnProperty(p)) {
+            let item: any = p;
+            let list: any = pending[p];
+            if (list.url === config.url + '&request_type=' + config.method) {
+                list.cancel();
+                pending.splice(item, 1);
+            }
+        }
+    }
+};
+
+const Service = axios.create(baseConfig);
+
+Service.interceptors.request.use(
+    req => {
+        removePending(req);
+        baseConfig.cancelToken = new axios.CancelToken((c) => {
+            pending.push({ url: baseConfig.url + '&request_type=' + baseConfig.method, cancel: c });
+        });
+        return baseConfig;
+    },
+    error => Promise.reject(error)
+);
+
+Service.interceptors.response.use(
+    res => {
+        removePending(res.config);
+        return res;
+    },
+    error => Promise.reject(error)
+);
+
+export default Service;
