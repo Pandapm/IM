1e29f5f4868ecbc2e2aebd2a297abbfeeddb9d3b
JunYing <junying@pinduoduo.com>
2018-09-14 11:52:06 +0800

    feat(更改页面): 更改侧边样式

--- src/components/MenuList/index.tsx
+++ src/components/MenuList/index.tsx
@@ -5,7 +5,6 @@ import MenuLink from './menu';
 import Main from '../Main';
 import { menuList, menuFeature, infoFeature } from '../../common/data';
 import './style.less';
-const logo = require('./favicon.png');
 
 const prefixCls = 'menu-wrapper'; 
 
@@ -29,10 +28,9 @@ const MenuList = () => (
                 clsName={`${prefixCls}-content-info`}
                 list={infoFeature}
             />
-            <a className={`${prefixCls}-footer`}>
-                <img src={logo} />
-                <p className={`${prefixCls}-footer-name`}>杨远</p>
-            </a>
+            <div className={`${prefixCls}-footer`}>
+                <p>杨远</p>
+            </div>
         </div>
         <Main />
     </div>
